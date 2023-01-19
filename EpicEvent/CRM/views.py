1872d1064a8e7db0a837461642377e6cb5123f18
Olrio <opm.rio@gmail.com>
2023-01-19 10:00:57 +0100

    différentes permissions pour les  actions de GET et de POST

--- EpicEvent/CRM/views.py
+++ EpicEvent/CRM/views.py
@@ -21,13 +21,22 @@ class MultipleSerializerMixin:
 class ClientViewset(MultipleSerializerMixin, ModelViewSet):
     serializer_class = ClientListSerializer
     detail_serializer_class = ClientDetailSerializer
-    permission_classes = [IsManagementTeam|IsSalesTeam|IsSupportTeam]
+    # permission_classes = [IsManagementTeam|IsSalesTeam|IsSupportTeam]
+
+
+    def get_permissions(self):
+        if self.request.method == 'GET' and "pk" not in self.request.parser_context["kwargs"]:
+            permission_classes =  [IsManagementTeam|IsSalesTeam|IsSupportTeam]
+        elif self.request.method == 'GET' and "pk" in self.request.parser_context["kwargs"]:
+            permission_classes =  [IsManagementTeam|IsSalesTeam, IsClientSalesContact]
+        elif self.request.method == 'POST':
+            permission_classes =  [IsManagementTeam|IsSalesTeam]
+        return [permission() for permission in permission_classes]
 
     def get_queryset(self):
         if "pk" not in self.request.parser_context["kwargs"]:
             return Client.objects.all()
         else:
-            self.permission_classes = [IsClientSalesContact]
             client_pk = self.request.parser_context["kwargs"]["pk"]
             try:
                 Client.objects.get(id=client_pk)
